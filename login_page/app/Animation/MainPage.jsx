81ee5d371d66a1dad0a3e2af2a2b385fd5cb8a5b
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2023-12-25 19:33:03 +0530

    improved_codeReadabilty

--- login_page/app/Animation/MainPage.jsx
+++ login_page/app/Animation/MainPage.jsx
@@ -12,7 +12,8 @@ export default function MainPage(props) {
   const [isHiddenRight, setIsHiddenRight] = useState(true); 
   const [overlayPositionF, setOverlayPositionF] = useState(50); // State to track the position of the overlay
   const [isHiddenLeftF, setIsHiddenLeftF] = useState(false); 
-  const [isHiddenRightF, setIsHiddenRightF] = useState(true)
+  const [isHiddenRightF, setIsHiddenRightF] = useState(true);
+  const [btnText, setBtnText] = useState("Login")
 
   // Function to move the overlay div
     const moveDiv = () => {
@@ -22,41 +23,88 @@ export default function MainPage(props) {
     setOverlayPositionF(prevPosition => prevPosition === 50 ? 0 : 50); // Toggle the position
     setIsHiddenLeftF(hidden => !hidden); // Toggle the visibility of the left div!isHidden) // Toggle the visibility of the left div
     setIsHiddenRightF(hidden =>!hidden);  // Toggle the visibility of the right div!isHidden) // Toggle the visibility of the right div
-
+    setBtnText(text =>text === "Login" ? "Sign in" : "Login" ); 
 };
+  //control gradient effects and transition timing here
+  const overlayStyle =    
+  {
+    left: `${overlayPosition}%`,
+    transition: 'all 0.7s ease-in-out',
+    /// make change here:
+    background: isHiddenLeftF ? 'linear-gradient(to right, #FF4B2B, #FF416C)' : 'linear-gradient(to right, #FF4B2B, #FF416C)'
+  }
+
+  const overLeftStyle =
+  {
+    transform: `translateX(${isHiddenLeft ? '-100%' : '0%'})`,
+    transition: 'transform 0.5s ease-in-out'
+
+  }
+
+  const overRightStyle =
+  {
+    transform: `translateX(${isHiddenRight ? '100%' : '0%'})`, 
+    transition: 'transform 0.5s ease-in-out'
+
+
+  }
+
+  const creaAccStyle = 
+  { 
+    
+     opacity: isHiddenLeftF ? 0 : 1 ,
+     transform: `translateX(${isHiddenLeftF ? '-100%' : '0%'})`, 
+     transition: 'all 0.5s ease-in-out' ,
+
+
+
+  }
+  const signInStyle =
+  {
+    opacity: isHiddenRightF ? 0 : 1 , 
+    transform: `translateX(${isHiddenRightF ? '100%' : '0%'})`,
+     transition: 'all 0.5s ease-in-out',
+    
+  }
+
 
   return (
     <div>
     
-          <div id='overlay' className=' h-full flex flex-col  justify-center items-center overflow-hidden absolute z-50 w-1/2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'
-           style={{ left: `${overlayPosition}%`, transition: 'left 0.7s ease-in-out' }} >
+          <div id='overlay' className=' h-full flex flex-col  justify-center items-center overflow-hidden absolute z-50 w-1/2 '
+           style={overlayStyle} >
 
             <div className='relative'>
 
-            <div id="left" className='absolute z-10'  style={{ transform: `translateX(${isHiddenLeft ? '-100%' : '0%'})`, transition: 'transform 0.5s ease-in-out' }}>
+            <div id="left" className='absolute z-10'  style={overLeftStyle}>
               <OverLeft />
             </div>
 
-            <div id="right" className='absoulute z-20'  style={{ transform: `translateX(${isHiddenRight ? '100%' : '0%'})`, transition: 'transform 0.5s ease-in-out' }}>
+            <div id="right" className='absoulute z-20'  style={overRightStyle}>
                <OverRight/>
             </div>
 
             </div>
-    
-            <BtnOver texts="Click me" handleclick={moveDiv} className="relative top-3/4"/>
+
+            <div className='mt-4'>
+            <BtnOver texts={btnText} handleclick={moveDiv}/>
+            </div>
+            
+
           </div>
+
 
           <div id='form' className=' h-full flex flex-col  justify-center items-center overflow-hidden absolute z-40 w-1/2'
            style={{ left: `${overlayPositionF}%`, transition: 'left 0.7s ease-in-out' }} >
 
             <div className='relative'>
 
-            <div id="left" className='absolute z-10'  style={{ opacity: isHiddenLeftF ? 0 : 1 , transform: `translateX(${isHiddenLeftF ? '-100%' : '0%'})`, transition: 'all 0.5s ease-in-out' }}>
+            <div id="left" className='absolute z-10'  style={creaAccStyle}>
                 
              <CreateAcc/>
             </div>
 
-            <div id="right" className='absoulute z-20 left-5'  style={{ opacity: isHiddenRightF ? 0 : 1 , transform: `translateX(${isHiddenRightF ? '100%' : '0%'})`, transition: 'all 0.5s ease-in-out' }}>
+            <div id="right" className='absoulute z-20 left-5'  style={signInStyle}>
             <SignIn/>
             </div>
 
